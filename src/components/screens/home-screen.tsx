57d2c6c8047dd1c71acb9fb75f46a47a4247c504
Sai Myo Myat <saimyomyat511@gmail.com>
2025-06-27 01:50:11 +0700

    feat: testing clipPath animation

--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -1,7 +1,12 @@
 import HomeSession from "../sessions/hero-session";
 
 const Home = () => {
-  return <HomeSession />;
+  return (
+    <>
+      <HomeSession />
+      <div className="w-screen h-screen bg-red-500"></div>
+    </>
+  );
 };
 
 export default Home;
--- src/components/sessions/hero-session.tsx
+++ src/components/sessions/hero-session.tsx
@@ -3,13 +3,17 @@ import gsap from "gsap";
 import { useCallback, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const HomeSession = () => {
   const navigate = useNavigate();
   const buttonRef = useRef<HTMLParagraphElement>(null);
 
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   useGSAP(() => {
     if (containerRef.current) {
@@ -25,6 +29,34 @@ const HomeSession = () => {
       );
     }
   }, [setOpen]);
+
+  useGSAP(() => {
+    gsap.set(containerRef.current, {
+      clipPath: "polygon(20% 0%, 80% 0%, 100% 100%, 0% 100%)",
+    });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+
+    tl.from(containerRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    }).to(
+      ".hero-title",
+      {
+        scale: 3,
+        rotateX: -45,
+        translateY: 200,
+        boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+      },
+      "<" // start at the same time as previous
+    );
+  }, []);
 
   const handleClick = useCallback(() => {
     if (containerRef.current) {
@@ -45,11 +77,11 @@ const HomeSession = () => {
   }, [navigate]);
 
   return (
-    <div ref={containerRef}>
+    <div ref={containerRef} className="bg-blue-300 w-screen h-dvh">
       {open && (
         <>
           <div className="flex flex-col items-center justify-center h-[80vh]">
-            <h1 className="text-4xl font-bold mb-4 text-blue-900">
+            <h1 className="hero-title z-10 text-4xl font-bold mb-4 text-blue-900">
               Welcome Home!
             </h1>
             <p className="text-lg text-gray-700">
