9cf80daf59929a99776a1dc9e972abb663ada0c4
Sai Myo Myat <saimyomyat511@gmail.com>
2025-07-19 18:26:13 +0700

    feat: bagan and inle sessions

---
+++ src/components/sessions/bagan.tsx
@@ -0,0 +1,15 @@
+const Bagan = () => {
+  return (
+    <div className="w-full h-dvh flex items-center justify-around">
+      <div>
+        <h1>Bagan</h1>
+      </div>
+      <img
+        src="/bagan.png"
+        className="object-fill object-center rounded-md w-1/2 h-[60vh]"
+      />
+    </div>
+  );
+};
+
+export default Bagan;
---
+++ src/components/sessions/inle.tsx
@@ -0,0 +1,15 @@
+const Inle = () => {
+  return (
+    <div className="w-full h-dvh flex items-center justify-around">
+      <img
+        src="/inle.jpg"
+        className="object-fill object-center rounded-md w-1/2 h-[60vh]"
+      />
+      <div>
+        <h1>Inle Lake</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Inle;
